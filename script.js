46fd8e84cebf02f68309ef1f1ccd6438cb1b37ac
Filip Nikolas Parak <filip.parak@yeself.com>
2021-03-30 08:09:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+(function (i, s, o, g, r, a, m) {
+  i['YesElfApi'] = r; i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.id = 'yesElf'; a.src = g; m.parentNode.insertBefore(a, m.nextSibling)
+})
+  (window, document, 'script', 'https://yeself-cloud-acc-app.brux.space:443/sdk/7aeb2b05-545a-4558-9a4c-7652e88b143e/app.ie.js', 'yesElf');
+yesElf('id', '7aeb2b05-545a-4558-9a4c-7652e88b143e');
+// yesElf('popups', true);
+// yesElf('segmentation', 'userId', 'testId');
+// yesElf('segmentation', 'roles', 'admin');
+// yesElf('segmentation', 'num', 5);
+// yesElf('segmentation', 'text', 'test');
+// yesElf('segmentation', 'date', '1.1.2021');
